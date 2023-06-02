ca9c45442ce7051f6ad3129fcc9eb09d914de178
Paul Dann <pgdann@gmail.com>
2023-06-02 14:18:39 +0100

    Add credo and dialyxir

--- mix.exs
+++ mix.exs
@@ -46,7 +46,9 @@ defmodule CqrsMemorySync.MixProject do
       {:telemetry_poller, "~> 1.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"}
+      {:plug_cowboy, "~> 2.5"},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3", only: [:dev], runtime: false}
     ]
   end
 
